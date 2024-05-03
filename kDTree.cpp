51d2a8d231385313bebeb7524fbd8f74566a2b61
phonghocode <ntp11892vt@gmail.com>
2024-05-03 18:55:26 +0700

    first commit

---
+++ kDTree.cpp
@@ -0,0 +1,19 @@
+#include "kDTree.hpp"
+
+/* TODO: You can implement methods, functions that support your data structures here.
+ * */
+kNN::kNN(int k) {
+
+}
+
+void kNN::fit(Dataset &X_train, Dataset &y_train) {
+
+}
+
+Dataset kNN::predict(Dataset &X_test) {
+    return X_test;
+}
+
+double kNN::score(const Dataset &y_test, const Dataset &y_pred) {
+    return 0.1;
+}
